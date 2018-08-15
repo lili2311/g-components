e80e301444fca85af33217331feb38283f6cdca5
Ændrew Rininsland <andrew.rininsland@ft.com>
2018-08-15 15:09:53 +0100

    Making styles.css export to top level

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -7,100 +7,78 @@
 import { resolve } from 'path';
 import BowerResolvePlugin from 'bower-resolve-webpack-plugin';
 import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
-import merge from 'webpack-merge';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin';
 
-module.exports = (mode = 'production') => {
-  const common = {
-    mode,
-    entry: ['./index.js'],
-    resolve: {
-      modules: ['bower_components', 'node_modules'],
-      plugins: [new BowerResolvePlugin()],
-      descriptionFiles: ['bower.json', 'package.json'],
-      mainFields: ['browser', 'main'],
-      alias: {
-        ftdomdelegate: 'dom-delegate',
-      },
+module.exports = (mode = 'production') => ({
+  mode,
+  output: {
+    libraryTarget: 'umd',
+    library: 'GComponents',
+    path: __dirname,
+    filename: 'dist/gcomponents.js',
+    sourceMapFilename: 'dist/gcomponents.map',
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true, // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+  },
+  entry: ['./index.js'],
+  resolve: {
+    modules: ['bower_components', 'node_modules'],
+    plugins: [new BowerResolvePlugin()],
+    descriptionFiles: ['bower.json', 'package.json'],
+    mainFields: ['browser', 'main'],
+    alias: {
+      ftdomdelegate: 'dom-delegate',
     },
-    devtool: 'source-map',
-    module: {
-      rules: [
-        {
-          test: /\.jsx?/,
-          loader: 'babel-loader',
-          exclude: /node_modules/,
-          options: {
-            presets: ['babel-preset-env', 'babel-preset-react'],
-            plugins: [
-              'add-module-exports' /* <-- wtfits */,
-              'transform-runtime',
-              'transform-class-properties',
-              'syntax-dynamic-import',
-            ],
-          },
-        },
-        {
-          test: /\.scss$/,
-          use: [
-            'style-loader',
-            'css-loader',
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true,
-                includePaths: ['bower_components'],
-              },
-            },
+  },
+  devtool: 'source-map',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['babel-preset-env', 'babel-preset-react'],
+          plugins: [
+            'add-module-exports' /* <-- wtfits */,
+            'transform-runtime',
+            'transform-class-properties',
+            'syntax-dynamic-import',
           ],
         },
-        {
-          test: /\.txt/,
-          loader: 'raw-loader',
-        },
-      ],
-    },
-  };
-
-  return [
-    merge(common, {
-      output: {
-        libraryTarget: 'umd',
-        library: 'GComponents',
-        path: resolve(__dirname, 'dist'),
-        filename: 'gcomponents.umd.js',
-        sourceMapFilename: 'gcomponents.umd.map',
       },
-    }),
-    merge(common, {
-      output: {
-        libraryTarget: 'commonjs2',
-        path: resolve(__dirname, 'dist'),
-        filename: 'gcomponents.cjs.js',
-        sourceMapFilename: 'gcomponents.cjs.map',
+      {
+        test: /\.scss$/,
+        use: [
+          mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              includePaths: ['bower_components'],
+            },
+          },
+        ],
       },
-    }),
-    merge(common, {
-      output: {
-        libraryTarget: 'umd',
-        library: 'GComponents',
-        path: resolve(__dirname, 'dist'),
-        filename: 'gcomponents.umd.min.js',
-        sourceMapFilename: 'gcomponents.umd.map',
+      {
+        test: /\.txt/,
+        loader: 'raw-loader',
       },
-      optimization: {
-        minimizer: [new UglifyJsPlugin()],
-      },
-    }),
-    merge(common, {
-      output: {
-        libraryTarget: 'commonjs2',
-        path: resolve(__dirname, 'dist'),
-        filename: 'gcomponents.cjs.min.js',
-        sourceMapFilename: 'gcomponents.cjs.map',
-      },
-      optimization: {
-        minimizer: [new UglifyJsPlugin()],
-      },
-    }),
-  ];
-};
+    ],
+  },
+});
