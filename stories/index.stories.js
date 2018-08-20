384df9e5353f3db2a0b0834daf53863a51be23f3
Ændrew Rininsland <andrew.rininsland@ft.com>
2018-08-20 17:08:02 +0100

    What

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,7 +4,7 @@
  */
 
 import React from 'react';
-
+// import PropTypes from 'prop-types';
 import { storiesOf, addDecorator } from '@storybook/react';
 import { withKnobs, text } from '@storybook/addon-knobs';
 import { withInfo } from '@storybook/addon-info';
